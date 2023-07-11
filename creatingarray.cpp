58e7c76ddd1dbc27901705e751a965e8119374fa
mittulofficial <111638490+mittulofficial@users.noreply.github.com>
2023-07-11 18:26:01 +0530

    Create creatingarray.cpp
    this is about declaring array taking input and giving output

---
+++ creatingarray.cpp
@@ -0,0 +1,26 @@
+//
+//  main.cpp
+//  Lecture5 2- Arrays pt 2
+//
+//  Created by MITTUL KUMAR on 11/jun/2023
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a[4],i;
+    cout<<"give 5 integers as input"<<endl;
+    for(i=0;i<5;i++){
+        cin>>a[i];
+    }
+    cout<<"the 5 given inputs are:"<<endl;
+for(i=0;i<5;i++)
+{
+        cout<<a[i];
+    }
+    
+    
+    
+    return 0;
+}
